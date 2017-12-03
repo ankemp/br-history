6af3e7c8c3959232a5508e66a28a9f6f624473b6
Andrew Kemp <andrew.kemp@viacomcontractor.com>
2017-12-03 08:55:40 -0500

    Adding firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
+
 import { HeaderModule } from './header/header.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
     AppRoutingModule,
     HeaderModule
   ],
