430c113cbf9036408f70db644723ebefae4600d6
Andrew Kemp <andrew@ankemp.com>
2017-12-06 20:31:21 -0500

    Circle CI stuff

---
+++ src/environments/firebase.ts
@@ -0,0 +1,8 @@
+export const firebase = {
+  apiKey: 'AIzaSyACfpkScHbzaLsNZRQ4SdiBeR6HkaRVE9w',
+  authDomain: 'br-history.firebaseapp.com',
+  databaseURL: 'https://br-history.firebaseio.com',
+  projectId: 'br-history',
+  storageBucket: 'br-history.appspot.com',
+  messagingSenderId: '499850358676'
+};
