fd8336d75a60d854e27e892a811dc3fd3b5af92a
Andrew Kemp <andrew@ankemp.com>
2017-12-08 12:05:06 -0500

    Removing firebase

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, QueryFn } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 
 import { Match } from '../models/match';
@@ -7,21 +6,6 @@ import { Match } from '../models/match';
 @Injectable()
 export class MatchService {
 
-  constructor(
-    private db: AngularFirestore,
-  ) { }
-
-  private matches(query?: QueryFn): AngularFirestoreCollection<Match> {
-    return this.db.collection('match', query);
-  }
-
-  public recent(playerId: string): Observable<Match[]> {
-    const query: QueryFn = (ref) => ref.where('rosters.participants.player.id', '==', playerId).orderBy('createdAt', 'desc').limit(5);
-    return this.matches().valueChanges();
-  }
-
-  public getMatch(matchId: string): Observable<Match> {
-    return this.matches().doc<Match>(matchId).valueChanges();
-  }
+  constructor() { }
 
 }
