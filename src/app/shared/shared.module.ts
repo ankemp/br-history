d1fb0a0611ec5d3700fb79977ccbbe641eed9968
Andrew Kemp <andrew@ankemp.com>
2017-12-18 12:18:55 -0500

    Adding loader component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatProgressSpinnerModule } from '@angular/material';
+
 import { SecToMinPipe } from './sec-to-min.pipe';
 import { ReduceStatPipe } from './reduce-stat.pipe';
 import { AssetRoutePipe } from './asset-route.pipe';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule
   ],
   declarations: [
     SecToMinPipe,
     ReduceStatPipe,
-    AssetRoutePipe
+    AssetRoutePipe,
+    LoaderComponent
   ],
   exports: [
     SecToMinPipe,
     ReduceStatPipe,
-    AssetRoutePipe
+    AssetRoutePipe,
+    LoaderComponent
   ]
 })
 export class SharedModule { }
