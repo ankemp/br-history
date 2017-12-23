c0f5e8a2d67f02ba83be32bd068009bd690ef1ac
Andrew Kemp <andrew@ankemp.com>
2017-12-22 19:43:00 -0500

    More updates to the profile summary page

---
+++ src/app/shared/winlose/winlose.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'brh-winlose',
+  templateUrl: './winlose.component.html',
+  styleUrls: ['./winlose.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class WinloseComponent {
+  @Input() wins: number;
+  @Input() losses: number;
+  @Input() showRate = true;
+
+  constructor() { }
+
+  get winRate(): string {
+    const totalPlayed = (this.wins || 0) + (this.losses || 0);
+    const winRate = (this.wins / totalPlayed * 100);
+    return !isNaN(winRate) ? `${winRate.toFixed(2)}%` : 'N/A';
+  }
+
+}
