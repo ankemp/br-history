08c64a82e5519427727626e7e4e21efce4b40466
Andrew Kemp <andrew@ankemp.com>
2017-12-27 15:24:03 -0500

    Adding GA event for champion sort

--- src/app/profile/champions/champions.component.ts
+++ src/app/profile/champions/champions.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { MatButtonToggleChange } from '@angular/material';
+import { Angulartics2 } from 'angulartics2';
 
 import { Player } from '@app/models';
 
@@ -11,13 +13,23 @@ import { Player } from '@app/models';
 export class ChampionsComponent {
   @Input() player: Player;
 
-  constructor() { }
+  constructor(
+    private ga: Angulartics2,
+  ) { }
 
   winRate(champion: any): string {
     const { characterWins, characterLosses } = champion;
     const totalPlayed = (characterWins || 0) + (characterLosses || 0);
     const winRate = (characterWins / totalPlayed * 100);
     return !isNaN(winRate) ? `${winRate.toFixed(2)}%` : 'N/A';
+  }
+
+  trackSort(event: MatButtonToggleChange): void {
+    const action = event.value.substr(1);
+    this.ga.eventTrack.next({
+      action,
+      properties: { category: 'champion sort' },
+    });
   }
 
 }
