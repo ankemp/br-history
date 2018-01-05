6cd2a6185ce1c2d36d2ff4533d6e213ee5cfdea7
Andrew Kemp <andrew@ankemp.com>
2018-01-04 23:59:08 -0500

    Cleaning up teams in team tab

--- src/app/profile/teams/teams.component.ts
+++ src/app/profile/teams/teams.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ChangeDetectionStrategy
+} from '@angular/core';
 
 import { Player, Team } from '@app/models';
 
@@ -8,10 +14,16 @@ import { Player, Team } from '@app/models';
   styleUrls: ['./teams.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TeamsComponent {
+export class TeamsComponent implements OnChanges {
   @Input() player: Player;
   @Input() teams: Team[];
+  rankedTeams: Team[];
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!!changes.teams) {
+      this.rankedTeams = changes.teams.currentValue.filter(team => team.teamType !== 'solo');
+    }
+  }
 }
