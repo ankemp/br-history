c0d101014eedcb221d533e0e4e7be9ddba0afde8
Andrew Kemp <andrew@ankemp.com>
2018-01-23 18:14:17 -0500

    Adding path redirect '' => 'page/home' (#38)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'page/home' },
   { path: 'profile', loadChildren: 'app/profile/profile.module#ProfileModule' },
   { path: 'match', loadChildren: 'app/match-details/match-details.module#MatchDetailsModule' },
   { path: 'page', loadChildren: 'app/page/page.module#PageModule' }
