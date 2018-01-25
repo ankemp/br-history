b757f907ce02f4c313834dc5a1959bc9174d3dfd
Andrew Kemp <andrew@ankemp.com>
2018-01-25 11:58:16 -0500

    Cleanup of header components

--- src/app/header/menu/menu.component.ts
+++ src/app/header/menu/menu.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Menu } from '@app/models';
 
 @Component({
   selector: 'brh-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuComponent {
   @Input() menu: Menu[];
