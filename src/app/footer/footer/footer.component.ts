9a62360226b0d486ee4af98d7822fe0e10bdcf1e
Andrew Kemp <andrew@ankemp.com>
2018-01-26 14:30:12 -0500

    Sitewide footer (#39)
    * Initial footer components
    
    * Cleanup of header components
    
    * Adding prebuild deploy helper script
    
    * Updating circleci process
    
    * Apparently prebuild is a real thing
    
    * Adding version to footer

---
+++ src/app/footer/footer/footer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { environment } from 'environments/environment';
+
+import * as menuActions from '@state/actions/menu';
+import * as fromMenu from '@state/menu';
+import { Menu } from '@app/models';
+
+@Component({
+  selector: 'brh-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  menu$: Observable<Menu[]>;
+  version = environment.version;
+
+  constructor(
+    private store: Store<Menu[]>
+  ) {
+    this.menu$ = store.select(fromMenu.getAllMenuItems);
+  }
+
+  ngOnInit() {
+    this.store.dispatch(new menuActions.Load);
+  }
+
+}
