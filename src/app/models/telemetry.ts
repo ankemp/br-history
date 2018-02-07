d77df1d021298af2cc0f8a05a9e1bafbc0587871
Andrew Kemp <andrew@ankemp.com>
2018-02-07 12:24:23 -0500

    Updating models

--- src/app/models/telemetry.ts
+++ src/app/models/telemetry.ts
@@ -3,10 +3,40 @@ import { Champion, PlayerStat, Round, Battlerite } from '@app/models';
 export interface Telemetry {
   battlerites: { [playerId: string]: Battlerite[] };
   roundStats: RoundStat[];
+  teamUpdate: TeamUpdate[];
 }
 
 export interface RoundStat extends Round {
   cursor: number;
-  time: number;
+  startTime: Date;
+  endTime: Date;
   stats: PlayerStat[];
+}
+
+export interface TeamUpdate {
+  cursor: number;
+  division: number;
+  divisionRating: number;
+  externalMatchID: string;
+  league: number;
+  losses: number;
+  matchID: string;
+  matchRegion: string;
+  mode: 'UNRANKED' | 'RANKED';
+  placementGamesLeft: number;
+  prevDivision: number;
+  prevDivisionRating: number;
+  prevLeague: number;
+  prevLosses: number;
+  prevPlacementGamesLeft: number;
+  prevWins: number;
+  rankingChangeType: string;
+  roundsLost: number;
+  roundsWon: number;
+  season: number;
+  teamID: string;
+  teamSize: number;
+  time: number;
+  userID: string;
+  wins: number;
 }
